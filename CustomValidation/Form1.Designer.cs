9e32c613359d698d0b6f056a9b17780c46c52338
PETERHO\PeterHo <peter_ho_249@outlook.com>
2019-01-02 09:03:13 +0700

    Show error

--- CustomValidation/Form1.Designer.cs
+++ CustomValidation/Form1.Designer.cs
@@ -28,13 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.errorTextbox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // errorTextbox
+            // 
+            this.errorTextbox.Location = new System.Drawing.Point(12, 12);
+            this.errorTextbox.Name = "errorTextbox";
+            this.errorTextbox.Size = new System.Drawing.Size(384, 199);
+            this.errorTextbox.TabIndex = 0;
+            this.errorTextbox.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 190);
+            this.ClientSize = new System.Drawing.Size(412, 224);
+            this.Controls.Add(this.errorTextbox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +52,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox errorTextbox;
     }
 }
 
---
+++ CustomValidation/Sample.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomValidation
+{
+    public class PersonalInformation
+    {
+        string _firstName;
+        string _lastName;
+        int _age;
+        Address _address;
+
+        public PersonalInformation(string firstName, string lastName, int age, Address address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Address = address;
+        }
+
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public int Age { get => _age; set => _age = value; }
+        public Address Address { get => _address; set => _address = value; }
+    }
+
+    public class Address
+    {
+        string _district;
+        string _province;
+        string _country;
+
+        public Address(string district, string province, string country)
+        {
+            District = district;
+            Province = province;
+            Country = country;
+        }
+
+        public string District { get => _district; set => _district = value; }
+        public string Province { get => _province; set => _province = value; }
+        public string Country { get => _country; set => _country = value; }
+    }
+
+    public class AddressValidate : AbstractValidator<Address>
+    {
+        public AddressValidate()
+        {
+            RuleFor(x => x.District).IsNotNull("District have to be not null.");
+            RuleFor(x => x.Province).IsNotNull("Province have to be not null.");
+            RuleFor(x => x.Country).IsNotNull("Country have to be not null.");
+        }
+    }
+
+    public class PersonalInformationValidate : AbstractValidator<PersonalInformation>
+    {
+        public PersonalInformationValidate()
+        {
+            RuleFor(x => x.FirstName).IsNotNull("First name have to be not null");
+            RuleFor(x => x.LastName).IsNotNull("Last name have to be not null");
+            RuleFor(x => x.Age).IsNotNull("Age have to be not null").IsGreaterThan(0, "Age have to be greater than 0");
+            RuleFor(x => x.Address, false).SetValidator(new AddressValidate());
+        }
+    }
+}
